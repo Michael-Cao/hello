3481e820e083db44143955ffd2ae1c8f8cd848f1
caomin2016@qq.com <caomin2016@qq.com>
2021-10-09 06:37:14 -0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Michael-Cao/hello
+
+go 1.17
+
+require github.com/Michael-Cao/greetings v0.0.0-20211009132229-e261fcb7de10
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Michael-Cao/greetings"
+)
+
+func main() {
+	fmt.Println(greetings.Hello("asdasd"))
+}
